cb88c8358401c608a7788f850275eeb59db75f2e
Nick Flint <nick.r.flint@gmail.com>
2021-09-26 11:21:50 -0400

    add api data service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,72 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserInformation } from './types/player-information';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  private REST_API_SERVER = 'http://localhost:8080/api/v1/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getAllUsersInformation(): Observable<UserInformation[]> {
+    return this.httpClient.get<UserInformation[]>(this.REST_API_SERVER +
+      'user/all');
+  }
+
+  public updateUserInformation(user: UserInformation): void {
+    this.httpClient.post<UserInformation>(
+      this.REST_API_SERVER + 'user/' + user.id + '/update',
+      {
+        id: user.id,
+        username: user.username,
+        playerLastName: user.playerLastName,
+        playerFirstName: user.playerFirstName,
+        position: user.position,
+        team: user.team,
+        discordUsername: user.discordUsername,
+        tpe: user.tpe,
+        joined: user.joined,
+        lastVisit: user.lastVisit,
+        active: user.active,
+        contacted: user.contacted,
+        want: user.want,
+        avoid: user.avoid,
+        drafted: user.drafted,
+        draftPick: user.draftPick,
+      });
+  }
+
+  public addUserInformation(user: UserInformation): void {
+    this.httpClient.post<UserInformation>(
+      this.REST_API_SERVER + 'user/add',
+      {
+        username: user.username,
+        playerLastName: user.playerLastName,
+        playerFirstName: user.playerFirstName,
+        position: user.position,
+        team: user.team,
+        discordUsername: user.discordUsername,
+        tpe: user.tpe,
+        joined: user.joined,
+        lastVisit: user.lastVisit,
+        active: user.active,
+        contacted: user.contacted,
+        want: user.want,
+        avoid: user.avoid,
+        drafted: user.drafted,
+        draftPick: user.draftPick,
+      });
+  }
+
+  public deleteUserInformation(id: number): void {
+    this.httpClient.post<number>(
+      this.REST_API_SERVER + 'user/' + id + '/delete',
+      {
+        id: id,
+      });
+  }
 }
--- src/app/view-all-players/view-all-players.component.ts
+++ src/app/view-all-players/view-all-players.component.ts
@@ -1,34 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UserInformation } from '../types/player-information';
+import { DataService } from '../data.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-view-all-players',
   templateUrl: './view-all-players.component.html',
   styleUrls: ['./view-all-players.component.css']
 })
-export class ViewAllPlayersComponent implements OnInit {
-  test: UserInformation = {
-    id: 1,
-    username: "test",
-    playerLastName: "last",
-    playerFirstName: "first",
-    position: "G",
-    team: "ANA",
-    discordUsername: "TEST#1231",
-    tpe: 155,
-    joined: "09/26/21",
-    lastVisit: "09/26/21",
-    active: true,
-    contacted: false,
-    want: false,
-    avoid: true,
-    drafted: false,
-    draftPick: -1,
-  };
+export class ViewAllPlayersComponent implements OnInit, OnDestroy {
+  allUserInformation: UserInformation[] = [];
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.dataService.getAllUsersInformation().pipe(takeUntil(this.destroy$))
+      .subscribe((data)=>{
+        console.log(data);
+        this.allUserInformation = data;
+      });
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    // Unsubscribe from the subject
+    this.destroy$.unsubscribe();
+  }
 }
